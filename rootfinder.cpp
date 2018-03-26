e9cb9d1c343989a10957eb65c994551b3e4b9596
nicamedrano <37525942+nicamedrano@users.noreply.github.com>
2018-03-26 15:22:29 +0800

    Update rootfinder.cpp

--- rootfinder.cpp
+++ rootfinder.cpp
@@ -5,11 +5,10 @@
 using namespace std;
 
 double horners(complex<double> poly[], complex<double> roots[], int order){
-    int n = order;
     complex<double> result;
     for(int j=0; j<order; j++){
-        result = poly[n];
-        for (int i=n-1; i>=0; --i){
+        result = poly[order];
+        for (int i=order-1; i>=0; --i){
             result = result*roots[j] + poly[i];
         }
         std::cout.precision(5);
