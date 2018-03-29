3f851190ef80b8aa60d7d728bca03c05345f5b1e
nicamedrano <37525942+nicamedrano@users.noreply.github.com>
2018-03-29 23:46:48 +0800

    input fxns included

--- main.cpp
+++ main.cpp
@@ -5,10 +5,33 @@
 #include "rootfinder.h"
 #include "math.cpp"
 #include <stdlib.h> //will find a way for rng soon
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 #define MAX_ORDER 20
+
+using namespace std;
+void getpoly(string fileName)
+{
+  ifstream polyFile;
+  int order;
+  polyFile.open(fileName.c_str());
+  if(polyFile.fail())
+  {
+    cout << "Error opening file!";
+    return;
+  }
+  polyFile>> order;
+  double polyArray[order-1];
+  for(int i=0; i <= order-1; i++)
+  {
+      polyFile>> polyArray[i];
+      cout<< polyArray[i] << endl;
+  }
+  polyFile.close();
+}
 
 int main(int argc, char** argv) 
 {
@@ -28,6 +51,26 @@ int main(int argc, char** argv)
 	}
 	Groot.printRoots();
 	Poly.deletePoly();
+	 string fileName;
+    if (argc==2)
+    {
+        fileName=argv[1];
+    }
+    else if(argc==1)
+    {
+        cout<<"Filename:";
+        cin>> fileName;
+    }
+    else
+    {
+        cout<< "Invalid input. Please only enter the program name along with the file you want to be read.\n e.g. polyroot fileName.txt";
+        return 0;
+    }
+    if(fileName.find(".txt") == string::npos)
+    {
+        fileName = fileName + ".txt";
+    }
+    getpoly(fileName);
 	
 	return 0;
 }
